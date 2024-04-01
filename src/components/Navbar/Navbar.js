8359f1096e5deee23caec7b385a65655af995527
toska27 <nemanjatoskic58@gmail.com>
2024-04-01 14:27:22 +0200

    hamburger menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
 import "./Navbar.css";
 
 const Navbar = ({ theme }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav>
-      <ul className="nav_list">
+      <button
+        className={theme === "light" ? "hamburger_menu" : "hamburger_menu_dark"}
+        onClick={toggleMenu}
+      >
+        <FaBars />
+      </button>
+      <ul
+        className={`nav_list ${theme === "light" ? "" : "nav_list_dark"} ${
+          isMenuOpen ? "show_menu" : ""
+        }`}
+      >
         <li className="nav_list-item">
           <a
             href="#projects"
